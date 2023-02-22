cc36573c2f427b1e3c69cc40a795703c42ef20a0
Kwame Bahena <kwame@informatux.net>
2023-02-21 20:25:14 -0600

    first commit

---
+++ path.py
@@ -0,0 +1,8 @@
+#/opt/homebrew/bin/python3
+import os
+out = os.path.basename("/Users/dan/Downloads")
+print(out)
+name = os.path.dirname("/Users/dan/Downloads")
+print(name)
+abs = os.path.isabs("/Users/dan/Downloads")
+print(abs)
