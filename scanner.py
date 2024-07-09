ec0ac8e524aa425377c4cb5d483f83a3a928779a
Kwame Bahena <kwame@informatux.net>
2024-07-08 20:36:38 -0600

    Converting scan into a function

--- scanner.py
+++ scanner.py
@@ -4,6 +4,7 @@ import subprocess
 
 network = input("Ingresa el rango de red que deseas escanear: ")
 
-scan = subprocess.run(["sudo", "nmap", "-sT", "-O", network])
+def scan():
+    range = subprocess.run(["sudo", "nmap", "-sT", "-O", network])
 
-print(scan.stdout)
+scan()
