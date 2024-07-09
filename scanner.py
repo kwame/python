155ef9614527951fa003459fdf091969b80c0798
Kwame Bahena <kwame@informatux.net>
2024-07-09 12:49:41 -0600

    Updating script, adding routine to check if nmap binary exists

--- scanner.py
+++ scanner.py
@@ -1,12 +1,26 @@
 import os
 import sys
 import subprocess
+import shutil
 
-nmap_binary = os.path.isfile(/)
-
-network = input("Ingresa el rango de red que deseas escanear: ")
+def is_executable(file_path):
+    executable_path = shutil.which(file_path)
+ 
+    if executable_path and os.access(executable_path, os.X_OK):
+        return True
+    else:
+        return False
 
 def scan():
     range = subprocess.run(["sudo", "nmap", "-sT", "-O", network])
 
-scan()
+
+file_to_check = "nmap"
+if is_executable(file_to_check):
+    print(f"El binario de {file_to_check} si existe en el sistema")
+    network = input("Ingresa el rango de red que deseas escanear: ")
+    scan()
+
+else:
+    print(f"{file_to_check} no está instalado, favor de instalarlo para proseguir.")
+
